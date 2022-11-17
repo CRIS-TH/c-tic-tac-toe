a8adaabc27e455a800bbb623b85c11420445c44c
Teacher Isaac <isaac@cris.ac.th>
2022-11-17 10:34:39 +0700

    main file

---
+++ main.c
@@ -0,0 +1,7 @@
+/*\
+|*| Tic Tac Toe
+|*| This is an interactive console game, implemented in C.
+|*| Created by ICT Elective students at Chiang Rai International School (CRIS)
+|*| Release to the Public Domain under CC0-v1.0.
+\*/
+
