8f758fbaac79678fc2ddcbd0b84d72247a83412f
Pheem <pheem@cris.ac.th>
2022-12-08 11:18:44 +0700

    Added print usage function; Demo board to be implemented

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #define BOARD_SIZE 9
 
 void print_board(char board[]);
+void usage();
 
 int main(void) {
 	
@@ -26,6 +27,22 @@ void print_board(char board[]){
 
 	for (i=0; i<BOARD_SIZE; i++)
 		printf("%c\n", board[i]);
-		
+
+	usage();
+
+}
+
+void usage() {
+	
+	char board[BOARD_SIZE] = {'1','2','3','4','5','6','7','8','9'};
+	
+	// TODO: Call print board function for demo array
+	// print_board(board)
+
+	printf(
+		"\nThe Number corresponds to your number pad on the keyboard.\n"
+		"By inputing number, you will fill the corresponding cell with your symbol.\n"
+		"You will win if you match 3 of your symbols horizontally, vertically, or diagonally.\n"
+	);
 
 }
