e867bf855c1c83ce14882fb1b5c213b51b36bd38
Pheem <pheem@cris.ac.th>
2023-01-13 09:45:34 +0700

    Added win_check function

--- main.c
+++ main.c
@@ -10,25 +10,30 @@
 
 void print_board(char board[]);
 void usage();
-char get_move(char player);
 void update_board(char board[], char s, char player);
+char get_move(char player);
+char win_check(char board[]);
 
 int main(void) {
-
+	
+	char testboard[BOARD_SIZE];
 	char player1 = 'X';
 	char player2 = 'O';
 	char demoboard[BOARD_SIZE] = {'7','8','9','4','5','6','1','2','3'};
 	char board[BOARD_SIZE];
 	for(int i = 0; i < BOARD_SIZE; ++i)
-		board[i] = ' ';
+		board[i], testboard[i] = 'X';
 	char currentplayer = player1;
 	char s;
 
-	print_board(demoboard);
+	print_board(testboard);
+	win_check(testboard);
+
+	/*print_board(demoboard);
 	usage();
 	s = get_move(currentplayer);
 	update_board(board, s, currentplayer);
-	print_board(board);
+	print_board(board);*/
 
 	return 0;
 }
@@ -59,7 +64,7 @@ char get_move(char player) {
 	} else {
 		printf("Player 2's turn. Input move: ");
 	}
-	
+	https://github.com/CRIS-TH/c-tic-tac-toe.git
 
 	do {
 		char c = getchar();
@@ -91,4 +96,22 @@ void update_board(char board[], char s, char player){
 	i = s;
 	i -= 49;
 	board[i] = player;
+}
+
+// Check if the game is over
+char win_check(char board[]) {
+	int amount = 0;
+	for(int i = 0; i < BOARD_SIZE; ++i) {
+		if(board[i] != ' '){
+			++amount;
+		}
+	}
+	
+	printf("%i", amount);
+
+	if (amount == 9) {
+		//TODO: Display tie message through the function
+		printf("\nTie\n");
+	} else if (board[0]!=' ' && board[1]!=' ' && board[2]!=' ')
+		printf("%c Win", board[0]);
 }
