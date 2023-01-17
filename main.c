d115ff3c80d01f6126ce43a996a13ec9b24f3157
Pheem <pheem@cris.ac.th>
2023-01-17 11:31:53 +0700

    Changed win_check arguments

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 
 #include <stdio.h>
 #define BOARD_SIZE 9
+#define EMPTY ' '
+#define TIE 0
+#define NOWINNER -1
 /*
 	TODO define symbolic constants for chars and use instead of literals throughout the program:
 
@@ -19,7 +22,7 @@ void print_board(char board[]);
 void usage();
 void update_board(char board[], char s, char player);
 char get_move(char player, char board[]);
-char win_check(char board[], char currentplayer);
+char win_check(char board[]);
 
 int main(void) {
 	
@@ -33,7 +36,7 @@ int main(void) {
 	char demoboard[BOARD_SIZE] = {'1','2','3','4','5','6','7','8','9'};
 	char board[BOARD_SIZE];
 	for(int i = 0; i < BOARD_SIZE; ++i)
-		board[i] = ' ';
+		board[i] = EMPTY;
 	char currentplayer = player1;
 	char s;
 
@@ -45,14 +48,7 @@ int main(void) {
 		update_board(board, s, currentplayer);
 		print_board(board);
 
-		/* TODO
-		if win_check is >= 0
-			print_winner(char returned by win_check)
-		else
-			keep playing
-		*/
-		if (win_check(board, currentplayer) != 0)
-			break;
+		printf("%i", win_check(board));
 
 		if (currentplayer == 'X')
 			currentplayer = 'O';
@@ -90,7 +86,6 @@ char get_move(char player, char board[]) {
 	} else {
 		printf("Player 2's turn. Input move: ");
 	}
-	https://github.com/CRIS-TH/c-tic-tac-toe.git
 
 	do {
 		char c = getchar();
@@ -138,38 +133,25 @@ void update_board(char board[], char s, char player){
 		-1 : no current winner
 */
 /* TODO we can remove the second parameter `player` */
-char win_check(char board[], char player) {
-	/*
-	TODO - the winning player can be determined from existing values
-
-	if (board[0] != EMPTY && board[0] == board[1] == board[2])
-		return board[0];
-	*/
-
+char win_check(char board[]) {
 	// Horizontal wins
-	if (board[0]==player && board[1]==player && board[2]==player)
-		//TODO: Call the game over function if the game is over
-		printf("%c Win\n", player);  /* TODO, don't do IO in a pure function - this is a side effect! */
-	else if (board[3]==player && board[4]==player && board[5]==player)
-		printf("%c Win\n", player);
-	else if (board[6]==player && board[7]==player && board[8]==player)
-		printf("%c Win", player);
+	if (board[0] != EMPTY && board[0] == board[1] ==board[2])
+		return board[0];  /* TODO, don't do IO in a pure function - this is a side effect! */
+	else if (board[3] != EMPTY && board[3] == board[4] == board[5])
+		return board[3];
+	else if (board[6] != EMPTY && board[6] == board[7] == board[8])
+		return board[6];
 	// Vertical wins
-	else if (board[0]==player && board[3]==player && board[6]==player)
-		printf("%c Win\n", player);
-	else if (board[1]==player && board[4]==player && board[7]==player)
-		printf("%c Win\n", player);
-	else if (board[2]==player && board[5]==player && board[8]==player)
-		printf("%c Win\n", player);
-	// Crossed wins
-	else if (board[0]==player && board[4]==player && board[8]==player)
-		printf("%c Win\n", player);
-	else if (board[2]==player && board[4]==player && board[6]==player)
-		printf("%c Win\n", player);
+	else if (board[0] != EMPTY && board[0] == board[3] == board[6])
+		return board[0];
+	else if (board[1] !=EMPTY && board[1] == board[4] == board[8])
+		return board[0];
+	else if (board[2] != EMPTY && board[2] == board[4] == board[6])
+		return board[2];
 	// Tie
-	else if (board[0]!=' '&&board[1]!=' '&&board[2]!=' '&&board[3]!=' '&&board[4]!=' '&&board[5]!=' '&&board[6]!=' '&&board[7]!=' '&&board[8]!=' ')
-		printf("Tie\n");
+	else if (board[0]!=EMPTY&&board[1]!=EMPTY&&board[2]!=EMPTY&&board[3]!=EMPTY&&board[4]!=EMPTY&&board[5]!=EMPTY&&board[6]!=EMPTY&&board[7]!=EMPTY&&board[8]!=EMPTY)
+		return TIE;
 	// Game not over yet
 	else
-		return 0;
+		return NOWINNER;
 }
